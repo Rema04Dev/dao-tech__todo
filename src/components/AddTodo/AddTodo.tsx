9e30fb9f3886d76c10cc9bb8bccde072a11a58ae
Roman <romansmerdyakov@gmail.com>
2023-05-25 23:00:12 +0600

    add Contoller

--- src/components/AddTodo/AddTodo.tsx
+++ src/components/AddTodo/AddTodo.tsx
@@ -1,27 +1,44 @@
 import { FC } from 'react';
-import { TextField, Button, Grid } from '@mui/material';
+import { TextField, Button, Grid, Box } from '@mui/material';
+import { useForm, Controller } from 'react-hook-form';
 const AddTodo: FC = () => {
+  const { register, handleSubmit, control } = useForm();
+  const onSubmit = (values: any) => console.log(values);
   return (
-    <Grid container>
-      <Grid item>
-        <TextField
-          autoComplete="off"
-          label="Enter a new task"
-          variant="outlined"
-        />
+    <Box component="form" onSubmit={handleSubmit(onSubmit)}>
+      <Grid container>
+        <Grid item>
+          <Controller
+            name="todo"
+            control={control}
+            render={({
+              field: { onChange, onBlur, value, name, ref },
+              fieldState: { invalid, isTouched, isDirty, error },
+              formState,
+            }) => (
+              <TextField
+                {...register('todo')}
+                autoComplete="off"
+                label="Enter a new task"
+                variant="outlined"
+              />
+            )}
+          />
+        </Grid>
+        <Grid item>
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            sx={{
+              height: '100%',
+            }}
+          >
+            Add
+          </Button>
+        </Grid>
       </Grid>
-      <Grid item>
-        <Button
-          fullWidth
-          variant="contained"
-          sx={{
-            height: '100%',
-          }}
-        >
-          Add
-        </Button>
-      </Grid>
-    </Grid>
+    </Box>
   );
 };
 
